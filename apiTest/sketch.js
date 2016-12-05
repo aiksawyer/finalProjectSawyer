d748b66e365fe518faa4d31d20b0e549d6f6307e
aiksawyer <wtfsawyer@172-16-19-66.DYNAPOOL.NYU.EDU>
2016-12-05 18:34:25 -0500

    testing api
    can’t get full result when running??

--- apiTest/sketch.js
+++ apiTest/sketch.js
@@ -1,22 +1,23 @@
-
-var json;
-var apiKey;
 var url;
 var query;
 var font;
+var comicsData;
+var issue;
 
-function preload(){
+function preload() {
   font = loadFont("data/BEBAS___.TTF");
+  var url = 'https://gateway.marvel.com/v1/public/characters/1009368/series?ts=1480980033&startYear=2009&orderBy=startYear&limit=100&apikey=4f804381e438abd7d337fe90bec41e4a&hash=cb22bb8e083ced8a01958588add95180';
+  comicsData = loadJSON(url);
 }
 
 function setup() {
-  createCanvas(500,500);
-  background(255);
-  
+  noLoop();
+
 }
 
 function draw() {
-  textFont(font, 25);
-  text("Data provided by Marvel.  2014 Marvel", 50, 50);
-  
+  textFont(font, 12);
+  var summary = comicsData.data.results;
+  text(summary, 50, 50);
+
 }
